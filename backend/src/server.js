a7c9bd35e231b261b25c22b9d7ee74cc13f87062
Blaicy <blaicymokaya@gmail.com>
2025-09-26 14:12:17 +0300

    Deployment changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,17 +4,22 @@ import dotenv from "dotenv"
 import notesRoutes from './routes/notesRoutes.js';
 import { connectDB } from './config/db.js';
 import rateLimiter from './middleware/rateLimiter.js';
+import path from "path"
 
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001
+const __dirname = path.resolve()
 
 //middleware
-app.use(cors({
+if(process.env.NODE_ENV !== "production"){
+   app.use(cors({
    origin:"http://localhost:5173"
    }))
+}
+
 app.use(express.json());
 app.use(rateLimiter)
 // app.use((req,res,next) => {
@@ -22,6 +27,16 @@ app.use(rateLimiter)
 // })
 
 app.use('/api/notes', notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  // Catch-all handler (SPA fallback)
+  app.use((req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+  });
+}
+
 
 connectDB().then(() => {
    app.listen(PORT, () => {
